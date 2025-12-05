28dbdb5c8c6c0cea7eaf37e1c01f625b0735d8ea
MySS0k <tokaj.szymon1@gmail.com>
2025-12-05 13:15:46 +0100

    Inicjalizacja zmiennych z xml do java

--- app/src/main/java/com/example/zadlek51225/MainActivity.java
+++ app/src/main/java/com/example/zadlek51225/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.zadlek51225;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText etImie, etNazwisko, etIleZnakow;
+    private Spinner spinner;
+    private CheckBox cbLitery, cbCyfry, cbZnaki;
+    private Button btnGeneruj,btnZatwierdz;
+
+    private String maleLitery = "qweryuiopasdfghjklzxcvbnm";
+    private String duzeLitery = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    private String znakiSpecjalne = "!@#$%^&*()-_=+";
+    private String cyfry = "1234567890";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        etImie = findViewById(R.id.etImie);
+        etNazwisko = findViewById(R.id.etNazwisko);
+        etIleZnakow = findViewById(R.id.etIleZnakow);
+        spinner = findViewById(R.id.spinner);
+        cbLitery = findViewById(R.id.cbLitery);
+        cbCyfry = findViewById(R.id.cbCyfry);
+        cbZnaki = findViewById(R.id.cbZnaki);
+        btnGeneruj = findViewById(R.id.btnGeneruj);
+        btnZatwierdz = findViewById(R.id.btnZatwierdz);
     }
 }
